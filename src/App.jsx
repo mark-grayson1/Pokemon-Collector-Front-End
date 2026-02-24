969907b91841cd8e73b38963c9f3766a2e2a91d4
ShivaniGanatra <shivaniganatra@live.com>
2026-02-24 09:58:29 +0000

    Removed unneccessary variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Nav from "./containers/Nav/Nav";
 import Dashboard from "./containers/Dashboard/Dashboard";
 
 function App() {
-	const [count, setCount] = useState(0);
+
 
 	return (
 		<main className="main">
