b0d4eb1cf6da4b8f6cc951468893be90c9170faa
Remi HOEPPE <remihoeppe@users.noreply.github.com>
2026-02-25 15:52:56 +0000

    Updating logout redirect to be root

--- src/containers/Nav/Nav.jsx
+++ src/containers/Nav/Nav.jsx
@@ -1,55 +1,55 @@
-import NavButton from "../../components/NavButton/NavButton";
-import "./Nav.scss";
-import { Link } from "react-router-dom";
+import NavButton from '../../components/NavButton/NavButton';
+import './Nav.scss';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
-	const handleLogout = async () => {
-		try {
-			await fetch(
-				"https://pokemon-collector-backend-production.up.railway.app/oauth2/authorization/logout",
-				{
-					method: "POST",
-					credentials: "include",
-				},
-			);
-		} catch (error) {
-			console.error("Logout failed:", error);
-		} finally {
-			window.location.href = "/landing";
-		}
-	};
+    const handleLogout = async () => {
+        try {
+            await fetch(
+                'https://pokemon-collector-backend-production.up.railway.app/oauth2/authorization/logout',
+                {
+                    method: 'POST',
+                    credentials: 'include',
+                },
+            );
+        } catch (error) {
+            console.error('Logout failed:', error);
+        } finally {
+            window.location.href = '/';
+        }
+    };
 
-	return (
-		<div className="Nav">
-			<section className="Nav_logo">
-				<img
-					className="Nav_img"
-					src="https://www.freepnglogos.com/uploads/pokemon-logo-text-png-7.png"
-					alt="Pokemon Logo"
-				/>
-			</section>
+    return (
+        <div className="Nav">
+            <section className="Nav_logo">
+                <img
+                    className="Nav_img"
+                    src="https://www.freepnglogos.com/uploads/pokemon-logo-text-png-7.png"
+                    alt="Pokemon Logo"
+                />
+            </section>
 
-			<section className="Nav_menu">
-				<Link to="/">
-					<NavButton name="Dashboard" />
-				</Link>
+            <section className="Nav_menu">
+                <Link to="/">
+                    <NavButton name="Dashboard" />
+                </Link>
 
-				<Link to="/selected">
-					<NavButton name="Selected" />
-				</Link>
+                <Link to="/selected">
+                    <NavButton name="Selected" />
+                </Link>
 
-				{}
-				<button
-					type="button"
-					onClick={handleLogout}
-					className="Nav_logoutBtn">
-					<NavButton name="Logout" />
-				</button>
-			</section>
+                {}
+                <button
+                    type="button"
+                    onClick={handleLogout}
+                    className="Nav_logoutBtn">
+                    <NavButton name="Logout" />
+                </button>
+            </section>
 
-			<section className="Nav_user">User</section>
-		</div>
-	);
+            <section className="Nav_user">User</section>
+        </div>
+    );
 };
 
 export default Nav;
